459b7b300f64f58a111c8178dad5618b1e95dcde
Drew <ayobamioduola13@gmail.com>
2024-11-27 17:00:19 +0100

    vendor_admin form

--- shoplit/customuser/admin.py
+++ shoplit/customuser/admin.py
@@ -7,10 +7,10 @@ from .models import CustomUser, Vendor
 
 @admin.decorators.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-   
-    
     add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    form = CustomUserCreationForm
+    model = CustomUser
+
     list_display = ('email', "is_staff", "last_login", "first_name", "last_name")
     list_filter = ("is_staff", "is_superuser", "is_active")
     search_fields = ("first_name", "last_name", "email")
@@ -23,11 +23,16 @@ class CustomUserAdmin(UserAdmin):
         }),
         ("Permission", {
             "classes": ("collapse",),
-            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+            "fields": ("is_active", 
+                       "is_staff", 
+                       "is_superuser", 
+                       "groups", 
+                       "user_permissions")
         }),
         ("Important Dates", {
             "classes" : ("collapse", ),
-            "fields": ("last_login", "date_joined")
+            "fields": ("last_login", 
+                       "date_joined")
         })
     )
     add_fieldsets = (
@@ -36,4 +41,23 @@ class CustomUserAdmin(UserAdmin):
             "classes": ("wide",),
             "fields": ("email", "password1", "password2"),
         }),
+    )
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('store_name', 'user', 'phone', 'joined_at')
+    search_fields = ('user', 'store_name', 'user__email')
+    ordering = ('store_name', 'user')
+    fieldsets = (
+        ("Store Info", {'fields': ('store_name', 
+                                   'user', 
+                                   'store_description',
+                                   'logo')}),
+                        
+        ("Contact", {
+            'fields': ('phone', 
+                       'address')}),
+        ("More Info", {
+            'fields': ('is_approved',)
+        })
     )
