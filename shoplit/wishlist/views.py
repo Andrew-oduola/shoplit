3205b61c0243dc9e07b3ce50b6426e00e9b6c207
Drew <ayobamioduola13@gmail.com>
2024-12-11 17:37:14 +0100

    chore: update requirements.txt

--- shoplit/wishlist/views.py
+++ shoplit/wishlist/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import WishListSerializer
 from .models import WishList
+
+
 # Create your views here.
 class WishListViewSet(viewsets.ModelViewSet):
     """
