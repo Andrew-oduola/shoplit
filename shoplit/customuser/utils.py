cb99d17f59a779ba00f7ab6929458633f8961b4f
Drew <ayobamioduola13@gmail.com>
2024-12-13 16:22:07 +0100

    chore: metadata csv added

--- shoplit/customuser/utils.py
+++ shoplit/customuser/utils.py
@@ -1,6 +1,20 @@
 from faker import Faker
-
+from django.conf import settings
 from django.contrib.auth.hashers import make_password
+
+import csv
+from pprint import pprint
+
+PRODUCTS_METADATA_CSV = settings.DATA_DIR / "ratings_Electronics.csv"
+
+def load_products_data(limit=10):
+    with open(PRODUCTS_METADATA_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for i, row in enumerate(reader):
+            pprint(row)
+            if i >= limit:
+                break
+            
 
 def get_fake_users(count=10):
     fake = Faker()
--- shoplit/shoplit/settings.py
+++ shoplit/shoplit/settings.py
@@ -20,6 +20,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
 
 
 # Quick-start development settings - unsuitable for production
