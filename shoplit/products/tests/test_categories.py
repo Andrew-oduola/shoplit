b7dde08999d18f1dcbb58d061bb293ec23bf7822
Drew <ayobamioduola13@gmail.com>
2024-12-26 09:46:23 +0100

    update test_categories.py

--- shoplit/products/tests/test_categories.py
+++ shoplit/products/tests/test_categories.py
@@ -1,11 +1,11 @@
-from rest_framework.test import APIClient
 from rest_framework import status
 
 import pytest
 from model_bakery import baker
 
 from customuser.models import CustomUser
-from products.models import Category, SubCategory, Product
+from products.models import Category
+
 
 @pytest.fixture
 def authenticate(api_client):
@@ -13,11 +13,21 @@ def authenticate(api_client):
         return api_client.force_authenticate(user=CustomUser(is_staff=is_staff)) 
     return do_authenticate
 
+
 @pytest.fixture
 def create_category(api_client):
     def do_create_category(category):
         return api_client.post('/api/products/categories/', category)
     return do_create_category
+
+
+@pytest.mark.django_db
+class TestListCategories:
+    def test_if_list_categories_returns_200(self, api_client):
+        response = api_client.get('/api/products/categories/')
+
+        assert response.status_code == status.HTTP_200_OK
+
 
 @pytest.mark.django_db
 class TestCreateCategory:
@@ -27,7 +37,6 @@ class TestCreateCategory:
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-
     
     def test_if_is_not_admin_return_403(self, authenticate, create_category):
         authenticate()
@@ -35,7 +44,6 @@ class TestCreateCategory:
         response = create_category({'name': 'a'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
-
 
     
     def test_if_data_is_invalid_returns_400(self, authenticate, create_category):
@@ -47,14 +55,15 @@ class TestCreateCategory:
         assert response.data['name'] is not None
 
 
-    
     def test_if_data_is_valid_returns_201(self, authenticate, create_category):
-        authenticate(True)
+        authenticate(is_staff=True)
 
         response = create_category({'name': 'a'})
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] is not None
+
+
 @pytest.mark.django_db
 class TestRetrieveCategory:
     def test_if_category_exist_returns_200(self, api_client):
@@ -64,7 +73,49 @@ class TestRetrieveCategory:
 
         assert response.status_code == status.HTTP_200_OK
 
+
     def test_if_category_did_not_exist_returns_404(self, api_client):
         response = api_client.get(f'/api/products/categories/wwwwwwwwwwww4ew/')
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+class TestUpdateCategory:
+    def test_update_put_category_returns_200(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        category = baker.make(Category)
+
+        response = api_client.put(f'/api/products/categories/{category.id}/', {'name': 'n', 'description': 'Description'})
+
+        assert response.status_code == status.HTTP_200_OK     
+
+
+    def test_if_update_category_returns_403_for_non_staff(self, authenticate, api_client):
+        authenticate()
+        category = baker.make(Category)
+
+        response = api_client.put(f'/api/products/categories/{category.id}/', {'name': 'update', 'description': 'desc'})
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    
+
+@pytest.mark.django_db
+class TestDeleteCategory:
+    def test_if_delete_category_returns_204(self, api_client, authenticate):
+        authenticate(is_staff=True)
+        category = baker.make(Category)
+
+        response = api_client.delete(f'/api/products/categories/{category.id}/')
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT  
+
+
+    def test_if_delete_category_returns_403_for_non_staff(self, api_client, authenticate):
+        authenticate()
+        category = baker.make(Category)
+
+        response = api_client.delete(f'/api/products/categories/{category.id}/')
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
