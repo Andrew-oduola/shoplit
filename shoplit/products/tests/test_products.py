d68de93cb31d612c5c8ccc0a1303a91c65fa0e4d
Drew <ayobamioduola13@gmail.com>
2024-12-28 01:49:24 +0100

    Update test_products.py: UpdateTest - Put & Patch

--- shoplit/products/tests/test_products.py
+++ shoplit/products/tests/test_products.py
@@ -224,4 +224,101 @@ class TestRetriveProduct:
         assert response.data['stock_quantity'] == product.stock_quantity
         # assert response.data['subcategory']['id'] == product.subcategory.id
 
+@pytest.mark.django_db
+class TestUpdateProduct:
+    def test_update_put_product_returns_200(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+        category = baker.make(Category)
+        subcategory = baker.make(SubCategory)
+        data = {}
+        response = api_client.put(f'/api/products/{product.id}/', 
+        {
+            "id": "0ebd1c22-4bab-4ee3-a329-977688260951",
+            "name": "free",
+            "description": "Why information gun make toward law us day. Education give guess state may six.\nStreet letter executive difference. Out some compare beyond note piece past town.",
+            "price": 812.94,
+            "stock_quantity": 8,
+            "category": category.id,
+            "subcategory": subcategory.id,
+            "average_rating": 0.0,
+            "review_count": 0.0
+        })
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_update_put_product_returns_403_for_non_staff(self, authenticate, api_client):
+        authenticate()
+        product = baker.make(Product)
+
+        response = api_client.put(f'/api/products/{product.id}/', {'name': 'update', 'description': 'desc', 'price': 123, 'stock_quantity': 1})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_update_put_product_returns_400_for_invalid_data(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+
+        response = api_client.put(f'/api/products/{product.id}/', {'name': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['name'] is not None
+
+    def test_if_update_put_product_returns_expected_data(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        category = baker.make(Category)
+        subcategory = baker.make(SubCategory, category=category)
+        product = baker.make(Product, category=category, subcategory=subcategory)
+        
+        data = {
+            "id": str(product.id),
+            "name": "free",
+            "description": "Why",
+            "price": "812.94",
+            "stock_quantity": 8,
+            "category": category.name,
+            "subcategory": str(subcategory.id),
+            "average_rating": 0.0,
+            "review_count": 0.0
+        }
+        response = api_client.put(f'/api/products/{product.id}/', data)
+
+        assert response.data['name'] == 'free'
+        assert response.data['description'] == 'Why'
+        assert response.data['price'] == Decimal("812.94")
+        assert response.data['stock_quantity'] == 8
+
+    def test_if_update_patch_product_returns_200(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+
+        response = api_client.patch(f'/api/products/{product.id}/', {'name': 'update'})
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_update_patch_product_returns_403_for_non_staff(self, authenticate, api_client):
+        authenticate()
+        product = baker.make(Product)
+
+        response = api_client.patch(f'/api/products/{product.id}/', {'name': 'update'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_update_patch_product_returns_400_for_invalid_data(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+
+        response = api_client.patch(f'/api/products/{product.id}/', {'name': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['name'] is not None
+
+    def test_if_update_patch_product_returns_expected_data(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+
+        response = api_client.patch(f'/api/products/{product.id}/', {'name': 'update'})
+        assert response.data['name'] == 'update'
+
+
 
