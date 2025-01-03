9aa635aa6d7e92d4505ad2fb20f5b11eb6f9b42f
Drew <ayobamioduola13@gmail.com>
2025-01-03 06:43:28 +0100

    feat: register notifications to admin

--- shoplit/notifications/admin.py
+++ shoplit/notifications/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Notifications
 
 # Register your models here.
+@admin.register(Notifications)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['title', 'message', 'created_at', 'updated_at', 'is_read']
+    list_filter = ['is_read', 'created_at']
+    search_fields = ['title', 'message']
+    list_editable = ['is_read']
+    list_display_links = ['title']
+    list_per_page = 10
+    list_max_show_all = 100
+
+
