80999f929f3be2026612a807291c7c5d2fb2be2d
Drew <ayobamioduola13@gmail.com>
2025-01-03 06:12:39 +0100

    feat: create model notifications

--- shoplit/notifications/models.py
+++ shoplit/notifications/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 
-
 # Create your models here.
+class Notifications(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
