f3ecbd2b4d878f470aa91342b4f73e2f98b32df9
Drew <ayobamioduola13@gmail.com>
2025-01-04 00:27:21 +0100

    feat: create signals.py to create notifications after an order is created

--- shoplit/orders/signals.py
+++ shoplit/orders/signals.py
@@ -1,10 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Order
+from orders.models import Order
+from notifications.models import Notifications
 
 @receiver(post_save, sender=Order)
-def notify_user_order_status(sender, instance, **kwargs):
-    if instance.status == 'DELIVERED':
-        # Send email notification to user
-        print(f"Order {instance.id} has been delivered to {instance.user.email}.")
+def create_notification(sender, instance, created, **kwargs):
+    if created:
+        Notifications.objects.create(
+            title='New Order',
+            message=f'Order {instance.id} has been created',
+            user=instance.user
+            )
 
+# @receiver(post_save, sender=Order)
+# def save_notification(sender, instance, **kwargs):
+#     instance.notifications.save()
